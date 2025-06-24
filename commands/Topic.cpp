bc0e3ff5e5e4ff43fe7b8013803278b2b54890ca
togauthi <togauthier23@gmail.com>
2025-06-24 12:27:00 +0200

    RPL_WHOREPLY

--- commands/Topic.cpp
+++ commands/Topic.cpp
@@ -54,8 +54,11 @@ int Topic::execute(const std::string &command, Client *cli) const
 		std::ostringstream oss;
 		oss << cli->getFd() << " " << channelName << " :" << channel->getTopic();
 		cli->sendMessage(oss);
+		std::ostringstream oss2;
+		oss2 << cli->getFd() << " " << channelName << " " << channel->whoSetTopic() << " " << channel->lastUpate(); 
+		cli->sendMessage(oss2);
 		return 1;
 	}
-	channel->setTopic(topic);
+	channel->setTopic(topic, cli->getUserName());
 	return 1;
 }
