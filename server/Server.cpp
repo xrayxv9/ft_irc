84aaba7c17a0733ec464a909c6557cd370c71c37
togauthi <togauthier23@gmail.com>
2025-06-30 10:00:15 +0200

    Fixing some leaks

--- server/Server.cpp
+++ server/Server.cpp
@@ -154,7 +154,10 @@ void Server::executeCommand()
 			else
 			{
 				std::cout << "left" << std::endl;
+				Client *cli;
+				cli = clients[it->fd];
 				clients.erase(it->fd);
+				delete cli;
 				fds.erase(it);
 				it--;
 			}
@@ -173,7 +176,7 @@ void Server::run()
 	char reading[1024];
 	Client *clientClass;
 
-	std::string welcomeMessage = "Welcome to the brand new onlyFans Server\n";
+	std::string welcomeMessage = "Welcome to the brand new onlyFans Server";
 	
 	while (g_running)
 	{
@@ -192,7 +195,7 @@ void Server::run()
 			}
 			std::cout << "-----" << clientFd << "-----" << reading << "----------" << std::endl;
 			clientClass = new Client(clientFd, getIndexClient(), *this, username, nickname);
-			send(clientClass->getFd(), welcomeMessage.c_str(), welcomeMessage.length(), 0);
+			clientClass->sendMessage(welcomeMessage);
 			createFd( clientFd );
 			clients[clientFd] = clientClass;
 			fds.data()->revents = 0;
